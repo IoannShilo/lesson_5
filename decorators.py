0d777bb8f4bd719bffa6a6821cf3a2426cf74026
ivashi <ivan-shilo.89@mail.ru>
2021-12-08 19:53:16 +0300

    cache done

---
+++ decorators.py
@@ -0,0 +1,24 @@
+def decorator(fn):
+    def wrapper(*args, **kwargs):
+        return fn(*args, **kwargs)
+    return wrapper
+
+
+def fabric_decorators(arg_decorator):
+    def decorator(fn):
+        def wrapper(*args, **kwargs):
+            return fn(*args, **kwargs)
+        return wrapper
+    return decorator
+
+@decorator
+def test_1(arg1):...
+
+
+@fabric_decorators(123)
+def test_2(arg1):...
+
+
+
+if __name__ == '__main__':
+    test_1(5)
